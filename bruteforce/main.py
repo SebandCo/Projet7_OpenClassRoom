54f7858334018ca1634ae56ab109dd892c69c964
SebandCo <113468072+SebandCo@users.noreply.github.com>
2023-04-19 15:19:06 +0200

    Analyse phase 2 ok

--- bruteforce/main.py
+++ bruteforce/main.py
@@ -1,113 +1,139 @@
 from objet_action import action
 from objet_action import possibilite
 
-from test import remise_a_zero
-
+# Action nul pour permettre le lancement de la boucle
+action0 = action(0,"Action-0",0,1)
 # Définition des différentes actions
-action1 = action(1,"Action-1",20,5,False,0)
-action2 = action(2,"Action-2",30,10,False,0)
-action3 = action(3,"Action-3",50,15,False,0)
-action4 = action(4,"Action-4",70,20,False,0)
-action5 = action(5,"Action-5",60,17,False,0)
-action6 = action(6,"Action-6",80,25,False,0)
-action7 = action(7,"Action-7",22,7,False,0)
-action8 = action(8,"Action-8",26,11,False,0)
-action9 = action(9,"Action-9",48,13,False,0)
+action1 = action(1,"Action-1",20,5)
+action2 = action(2,"Action-2",30,10)
+action3 = action(3,"Action-3",50,15)
+action4 = action(4,"Action-4",70,20)
+action5 = action(5,"Action-5",60,17)
+action6 = action(6,"Action-6",80,25)
+action7 = action(7,"Action-7",22,7)
+action8 = action(8,"Action-8",26,11)
+action9 = action(9,"Action-9",48,13)
+action10 = action(10,"Action-10",34,27)
+action11 = action(11,"Action-11",42,17)
+action12 = action(12,"Action-12",110,9)
+action13 = action(13,"Action-13",38,23)
+action14 = action(14,"Action-14",14,1)
+action15 = action(15,"Action-15",18,3)
+action16 = action(16,"Action-16",8,8)
+action17 = action(17,"Action-17",4,12)
+action18 = action(18,"Action-18",10,14)
+action19 = action(19,"Action-19",24,21)
+action20 = action(20,"Action-20",114,18)
 
 # Définition des paramètres de base
-liste_action = [action1, action2, action3, action4, action5, action6, action7, action8, action9]
-action_max = len(liste_action)-1
-montant_maximum = 200
-montant_actuel = 0
+liste_action = [action0,
+                action1,
+                action2,
+                action3,
+                action4,
+                action5,
+                action6,
+                action7,
+                action8,
+                action9,
+                action10,
+                action11,
+                action12,
+                action13,
+                action14,
+                action15,
+                action16,
+                action17,
+                action18,
+                action19,
+                action20]
 numero_possibilite = 1
 simulation = {}
-# Simulation 0
-simulation[0] =[0]
+
+# Définition des paramètres montant
+montant_provisoire = 0
+montant_maximum = 500
+
+# Définition des paramètre pour les simulations valides
+numero_possible = 1
+simulation_valide = {}
+
 # Initialisation de la première possibilité
 simulation[numero_possibilite] = []
 
 
 def rajout_action(liste_action,
-                  montant_maximum,
-                  montant_actuel,
-                  numero_possibilite,
+                  action_precedente,
                   simulation,
-                  rang_actuel):
+                  possibilite):
     for action in liste_action:
-        # On regarde si l'action est déjà utilisé
-        if action.utilisee == False:
-            montant_intermediaire = montant_actuel+ action.montant
-            # Si le montant est inférieur ou égale au montant maximum
-            if montant_intermediaire <= montant_maximum:
-                # On incrémente le nouveau montant_actuel
-                montant_actuel = montant_intermediaire
-                # On rajoute l'action à l'historique de la simulation
-                simulation[numero_possibilite].append(action.numero)
-                # On indique que l'action a été utilisée
-                action.utilisee = True
-                # On indique à quel rang
-                action.boucle_utilisee = rang_actuel
-                # On incrémente le rang
-                rang_actuel += 1
-
-        # Si nous somme sur la dernière action
-        if action.numero == len(liste_action) :
-            # Controle de la simulation : Si la simulation actuel, reprend la simulation précédente -1
-            # alors elle n'apporte rien de mieux. On peux la supprimer
-            simulation_controle = []
-            simulation_controle = (simulation[numero_possibilite - 1]) [0:((len(simulation[numero_possibilite-1])-1))]
-            if simulation[numero_possibilite] != simulation_controle:
-                print(simulation[numero_possibilite])
-                # On crée une nouvelle simulation
-                numero_possibilite += 1
-                simulation[numero_possibilite]=[]
-                # Si la derniere action n'a pas été utilisé
-                if action.utilisee == False:
-                    # On relance le programme à partir du rang n-1
-                    rang_choisi = rang_actuel - 1
-                # Si la dernière action a été utilisé
-                else:
-                    # On relance le programme à partir du rang n-2
-                    rang_choisi = rang_actuel - 2
+        if action.numero > action_precedente.numero:
+            simulation[possibilite].append(action)
+            global numero_possibilite 
+            numero_possibilite += 1
+            if action.numero != len(liste_action)-1:
+                simulation[numero_possibilite]=creation(simulation[possibilite])
             else:
-                # On relance le programme à partir du rang n-2
-                    rang_choisi = rang_actuel - 2
-            # On reprend l'historique de la dernière simulation
-            simulation[numero_possibilite] = remise_a_zero (liste_action,
-                                                            simulation[numero_possibilite],
-                                                            simulation[numero_possibilite-1],
-                                                            rang_choisi)
-            # Ensuite on relance le programme
-            montant_actuel = calcul_montant(liste_action,simulation[numero_possibilite])
+                simulation[numero_possibilite]=[]
             rajout_action(liste_action,
-                          montant_maximum,
-                          montant_actuel,
-                          numero_possibilite,
+                          action,
                           simulation,
-                          rang_choisi)
+                          numero_possibilite)
+            return ()
 
+# Reprise de l'historique de l'action précedente
+def creation (simul_precedente):
+    simulation = []
+    for i in simul_precedente:
+        simulation.append(i)
+    return simulation
 
-def calcul_montant (liste_action, simulation):
-    montant_actuel = 0
-    # Récupere les actions de la simulation
-    for action in simulation:
-        # Rajoute le montant des actions au montant courant
-        montant_actuel += liste_action[action-1].montant
-    return montant_actuel
-
-
-def remise_a_zero(liste_action,simulation_actuel, simulation_precedente, rang_choisi):
-    simulation_actuel = simulation_precedente [0 : (rang_choisi - 1)]
-    for action in liste_action:
-        if action.boucle_utilisee > rang_choisi:
-            action.utilisee = False
-    return simulation_actuel
+# Début du programme
+print (f"Début de l'analyse des {len(liste_action)-1} actions")
 
 # Boucle de départ
-for action in liste_action:
-    # Tant que le montant actuel est inférieur au montant de départ, on boucle
-    rajout_action(liste_action, montant_maximum, montant_actuel, numero_possibilite, simulation,1)
+for action_actuelle in liste_action:
+    rajout_action(liste_action,
+                  action_actuelle,
+                  simulation,
+                  numero_possibilite)
+
+# Premier compte-rendu
+print (f"Il y a {numero_possibilite-1} combinaisons d'action possible")
+
+for possibilites in simulation:
+    # Rajout du montant de l'action au montant provisoire
+    for action_possible in simulation[possibilites]:
+        montant_provisoire += action_possible.montant
+
+    # Controle si le montant_provisoire est inférieur au montant possible
+    if montant_provisoire <= montant_maximum:
+        simulation_valide[numero_possible] = possibilite (montant_provisoire,
+                                                          simulation[possibilites],
+                                                          0)
+        for action_valide in simulation_valide[numero_possible].liste_action:
+            simulation_valide[numero_possible].benefice += action_valide.benefice
+
+        numero_possible += 1
+    
+    # Remise à zéro du montant_provisoire avant la prochaine boucle
+    montant_provisoire = 0
+
+# Deuxième compte-rendu
+print (f"Sur les {numero_possibilite - 1} combinaison possible"
+       f"Seules {numero_possible - 1} sont conforme avec la limite de {montant_maximum} euros")
+
+# Analyse du benefice attendu
+meilleur_valeur = 0
+meilleur_benefice = 0
+
+# Recherche du meilleur bénéfice
+for action_possible in simulation_valide:
+    # Si deux bénéfices sont équivalent, recherche le plus petit investissement
+    if simulation_valide[action_possible].benefice == meilleur_benefice:
+        if simulation_valide[action_possible].
+
+
 
 
 print("fin du programme")
-
--- bruteforce/test.py
+++ bruteforce/test.py
@@ -1,28 +1,71 @@
-# Rajout de l'action à la simulation
-def rajout_action(action_max, action, simulation, montant_depart):
-    if action.utilisee == False:
-        montant_intermediaire = simulation.montant + action.montant
-        if montant_intermediaire <= montant_depart:
-            # Modifie les paramètres de l'action (numéro de boucle et utilisation)
-            action.boucle_utilisee = simulation.numero_boucle
-            action.utilisee = True
-            # Modifie les paramètres de la simulation en cours
-            simulation.montant = montant_intermediaire
-            simulation.liste_action.append(action)
-            simulation.benefice = simulation.benefice + action.benefice
-            simulation.numero_boucle = simulation.numero_boucle + 1
-            # Si le montant de départ est atteint, on arrete la boucle
-            if montant_intermediaire == montant_depart:
-                simulation.fin_de_boucle = True
-        else:
-            simulation.fin_de_boucle = True
+from objet_action import action
 
-    # Si on est sur la dernière action, on arrete la boucle
-    if int(action.nom[-1]) == action_max:
-            simulation.fin_de_boucle = True
+# Action nul pour permettre le lancement de la boucle
+action0 = action(0,"Action-0",0,1,False)
+# Définition des différentes actions
+action1 = action(1,"Action-1",20,5,False)
+action2 = action(2,"Action-2",30,10,False)
+action3 = action(3,"Action-3",50,15,False)
+action4 = action(4,"Action-4",70,20,False)
+action5 = action(5,"Action-5",60,17,False)
+action6 = action(6,"Action-6",80,25,False)
+action7 = action(7,"Action-7",22,7,False)
+action8 = action(8,"Action-8",26,11,False)
+action9 = action(9,"Action-9",48,13,False)
+
+# Définition des paramètres de base
+liste_action = [action0,
+                action1,
+                action2,
+                action3,
+                action4,
+                action5,
+                action6,
+                action7,
+                action8,
+                action9]
+numero_possibilite = 1
+simulation = {}
+
+# Initialisation de la première possibilité
+simulation[numero_possibilite] = []
 
 
-def remise_a_zero(liste_action, numero_boucle):
+def rajout_action(liste_action,
+                  action_precedente,
+                  simulation,
+                  possibilite):
     for action in liste_action:
-        if action.boucle_utilisee > numero_boucle:
-            action.utilisee = False
+        if action.numero > action_precedente.numero:
+            simulation[possibilite].append(action)
+            print(simulation[possibilite])
+            global numero_possibilite 
+            numero_possibilite += 1
+            if action.numero != len(liste_action)-1:
+                simulation[numero_possibilite]=creation(simulation[possibilite])
+            else:
+                simulation[numero_possibilite]=[]
+            rajout_action(liste_action,
+                          action,
+                          simulation,
+                          numero_possibilite)
+            return ()
+
+# Reprise de l'historique de l'action précedente
+def creation (simul_precedente):
+    simulation = []
+    for i in simul_precedente:
+        simulation.append(i)
+    return simulation
+
+
+# Boucle de départ
+for action_actuelle in liste_action:
+    rajout_action(liste_action,
+                  action_actuelle,
+                  simulation,
+                  numero_possibilite)
+
+
+
+print("fin du programme")
